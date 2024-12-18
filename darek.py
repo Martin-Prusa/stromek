6cefaa8dd4d9a54b5b870c50127ee7caa054221b
Průša Martin <martinprusa004@seznam.cz>
2024-12-18 14:57:16 +0100

    fix velikosti darku

--- darek.py
+++ darek.py
@@ -1,5 +1,5 @@
 def vykresli_darek(st, x, y, velikost=1, barva_darku=(1,0,0), barva_masle=(1,1,0)):
-    st.Kvadr(barva=barva_darku, x=x,y=y)
+    st.Kvadr(barva=barva_darku, x=x,y=y, sx=velikost, sy=velikost)
     st.Kvadr(barva=barva_masle, x=x,y=y, sx=1.1*velikost, sy=0.1*velikost, sz=1.1*velikost, rx=90)
     st.Kvadr(barva=barva_masle, x=x,y=y, sx=1.1*velikost, sy=0.1*velikost, sz=1.1*velikost, rz=90)
     st.Koule(x=x, y=y+velikost*0.7, sx=0.3*velikost, sy=0.5*velikost, barva=barva_masle)
