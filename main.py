e4e1b94caf8c1cef61bd4c8dc7c4c7abfde54e67
Průša Martin <martinprusa004@seznam.cz>
2024-12-18 15:13:21 +0100

    funkce pro vykreslení stromu

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import stavitelka as st
 from darek import vykresli_darek
+from stromek_a_zem import vykresli_stromek_a_zem
 
-vykresli_darek(st, 1, 1, 1)
-vykresli_darek(st, 3, 1, 1, barva_darku=(1,1,0), barva_masle=(1, 0, 0))
-vykresli_darek(st, 5, 1, 1, barva_darku=(0,0,1), barva_masle=(1, 0, 0), velikost=1.3)
+vykresli_stromek_a_zem(st)
+vykresli_darek(st, 1, -2.5, -2)
+vykresli_darek(st, -2, -2.5, 0, barva_darku=(1,1,0), barva_masle=(1, 0, 0))
+vykresli_darek(st, 2, -2.5, 1, barva_darku=(0,0,1), barva_masle=(1, 0, 0), velikost=1.3)
 
 st.ZobrazScenu()
--- stromek_a_zem.py
+++ stromek_a_zem.py
@@ -1,13 +1,11 @@
-import stavitelka as st
-
 # Vytvoření jehlanu na různých pozicích
-difsxsz = 0.1
-dify = 0.2
-for i in range(51):
-    st.Valec(y=0+i*dify, z=0, sx=5-i*difsxsz, sy=0.2, sz=5-i*difsxsz, barva=(0,1,0))
-# Zobrazení scény
-st.Valec(y=0, z=0, sx=1, sy=3, sz=1, barva=(0.5,0.2,0))
+def vykresli_stromek_a_zem(st):
+    difsxsz = 0.1
+    dify = 0.2
+    for i in range(51):
+        st.Valec(y=0+i*dify, z=0, sx=5-i*difsxsz, sy=0.2, sz=5-i*difsxsz, barva=(0,1,0))
+    # Zobrazení scény
+    st.Valec(y=0, z=0, sx=1, sy=3, sz=1, barva=(0.5,0.2,0))
 
-st.Kvadr(x=0,y=-3,z=0, sx=10, sy=0.01, sz=10, rx=0, ry=0, rz=0, barva=(0,1,0), vaha=0)
-st.ZobrazScenu()
+    st.Kvadr(x=0,y=-3,z=0, sx=10, sy=0.01, sz=10, rx=0, ry=0, rz=0, barva=(0,1,0), vaha=0)
 
